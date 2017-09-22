e66cac1781aab797fa017f555cdf9f9bb3c9b534
Philip <philipkarpovich@gmail.com>
2017-09-22 16:37:20 +0300

    init

--- src/fsm.js
+++ src/fsm.js
@@ -1,61 +1,83 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+  /**
+   * Creates new FSM instance.
+   * @param config
+   */
+  constructor(config) {
+    this.state = {
+      sleepeng: false,
+      hungry:   false,
+      mormal:   false,
+      busy:     false
+    }
+  }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+  /**
+   * Returns active state.
+   * @returns {String}
+   */
+  getState() {
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+  }
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+  /**
+   * Goes to specified state.
+   * @param state
+   */
+  changeState(state) {
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+  }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+  /**
+   * Changes state according to event transition rules.
+   * @param event
+   */
+  trigger(event) {
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+  }
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+  /**
+   * Resets FSM state to initial.
+   */
+  reset() {
+
+  }
+
+  /**
+   * Returns an array of states for which there are specified event transition rules.
+   * Returns all states if argument is undefined.
+   * @param event
+   * @returns {Array}
+   */
+  getStates(event) {
+
+  }
+
+  /**
+   * Goes back to previous state.
+   * Returns false if undo is not available.
+   * @returns {Boolean}
+   */
+  undo() {
+
+  }
+
+  /**
+   * Goes redo to state.
+   * Returns false if redo is not available.
+   * @returns {Boolean}
+   */
+  redo() {
+
+  }
+
+  /**
+   * Clears transition history
+   */
+  clearHistory() {
+
+  }
 }
 
 module.exports = FSM;
-
-/** @Created by Uladzimir Halushka **/
